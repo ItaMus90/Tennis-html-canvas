2694d9c853fdb622827ad1a61cd4c109091dc08d
ItaMus90 <itamarmuslvi@gmail.com>
2018-02-16 01:44:18 +0200

    Bouncing the ball with two side of the screen and create method colorRact for improve the code and dont using hard code

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var canvas;
 var canvasContext;
 var ballX = 50;
+var ballSpeedX = 10;
 
 /*
 window.onload this mean after the html load then the js come
@@ -18,14 +19,29 @@ window.onload = function (){
 };
 
 function moveEverything(){
-	ballX += 5;
+	ballX += ballSpeedX;
+
+	if(ballX < 0){
+		ballSpeedX = -ballSpeedX;
+	}
+
+	if(ballX > canvas.width){
+		ballSpeedX = -ballSpeedX;
+	}
 }
 
 function drawEverything(){
-	canvasContext.fillStyle = 'black';
-	canvasContext.fillRect(0,0,canvas.width,canvas.height);
-	canvasContext.fillStyle = 'white';
-	canvasContext.fillRect(0,210,10,100);
-	canvasContext.fillStyle = 'red';
-	canvasContext.fillRect(ballX,100,10,10);
+	//Next line blanks out the screen with black
+	colorRect(0,0,canvas.width,canvas.height, 'black');
+
+	//This is the left player paddle
+	colorRect(0,210,10,100, 'white');
+	
+	//Next line draw the ball
+	colorRect(ballX,100,10,10, 'red');
+}
+
+function colorRect(leftX, topY, width, height, drawColor){
+	canvasContext.fillStyle = drawColor;
+	canvasContext.fillRect(leftX,topY,width,height);
 }
