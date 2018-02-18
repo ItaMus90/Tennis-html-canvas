ff78d7a077d44d59a9f1884ce633bfb257d951ee
ItaMus90 <itamarmuslvi@gmail.com>
2018-02-19 01:06:28 +0200

    Find the mouse position relative And Set paddle's position value whenever mouse moves

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
 var canvas;
 var canvasContext;
 var ballX = 50;
+var ballY = 50;
 var ballSpeedX = 10;
+var ballSpeedY = 4;
+
+var paddle1Y = 250;
+const PADDLE_HIGEHT = 100;
+
+/**
+ * Find the mouse position relative 
+ * to game canvas
+ */
+function calculateMousePos(evt){
+	var rect = canvas.getBoundingClientRect();
+	var root = document.documentElement;
+	var mouseX = evt.clientX - rect.left - root.scrollLeft;
+	var mouseY = evt.clientY - rect.top - root.scrollTop;
+
+	return {
+		x: mouseX,
+		y: mouseY
+	};
+}
 
 /*
 window.onload this mean after the html load then the js come
 */
 window.onload = function (){
-	console.log('Hello Worls!');
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
 
@@ -16,10 +36,18 @@ window.onload = function (){
 		moveEverything();
 		drawEverything();
 	}, 1000/framesPerSecond);
+	
+	// Set paddle's position value whenever mouse moves
+	canvas.addEventListener('mousemove', function(evt){
+		var mousePos = 	calculateMousePos(evt);
+		paddle1Y = mousePos.y - (PADDLE_HIGEHT/2);
+
+	});
 };
 
 function moveEverything(){
 	ballX += ballSpeedX;
+	ballY += ballSpeedY;
 
 	if(ballX < 0){
 		ballSpeedX = -ballSpeedX;
@@ -28,6 +56,13 @@ function moveEverything(){
 	if(ballX > canvas.width){
 		ballSpeedX = -ballSpeedX;
 	}
+	if(ballY < 0){
+		ballSpeedY = -ballSpeedY;
+	}
+
+	if(ballY > canvas.height){
+		ballSpeedY = -ballSpeedY;
+	}
 }
 
 function drawEverything(){
@@ -35,10 +70,17 @@ function drawEverything(){
 	colorRect(0,0,canvas.width,canvas.height, 'black');
 
 	//This is the left player paddle
-	colorRect(0,210,10,100, 'white');
+	colorRect(0,paddle1Y,10,PADDLE_HIGEHT, 'white');
 	
 	//Next line draw the ball
-	colorRect(ballX,100,10,10, 'red');
+	colorCircle(ballX,ballY,10,'white')
+}
+
+function colorCircle(centerX, centerY, radius, drawColor){
+	canvasContext.fillStyle =  drawColor;
+	canvasContext.beginPath();
+	canvasContext.arc(centerX, centerY, radius, 0,Math.PI*2,true);
+	canvasContext.fill();
 }
 
 function colorRect(leftX, topY, width, height, drawColor){
